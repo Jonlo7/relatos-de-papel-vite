8181f9b7eaa871df096c3af720a6733bf3d8ebeb
Jon Lopez <jonlopez@opendeusto.es>
2024-12-24 17:30:32 +0100

    style: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SearchBar } from "./SearchBar";
 import { Cart } from "./Cart";
+import { useCart } from "../hooks/useCart";
 import HomeIcon from "../assets/icons/home.png";
 import UserIcon from "../assets/icons/user.png";
 import CartIcon from "../assets/icons/cart.png";
@@ -10,10 +11,13 @@ import "../styles/Layout.css";
 
 export const Header = () => {
     const [isCartVisible, setIsCartVisible] = useState(false);
+    const { cart } = useCart();
 
     const toggleCartVisibility = () => {
         setIsCartVisible(!isCartVisible);
     };
+
+    const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
 
     return (
         <header className="header">
@@ -30,8 +34,9 @@ export const Header = () => {
                 <button className="header-button">
                     <img src={UserIcon} alt="Perfil" />
                 </button>
-                <button className="header-button" onClick={toggleCartVisibility}>
+                <button className="header-button" id="cart-num" onClick={toggleCartVisibility}>
                     <img src={CartIcon} alt="Carrito" />
+                    {cartItemCount > 0 && <span className="cart-count">{cartItemCount}</span>}
                 </button>
             </div>
             <Cart isVisible={isCartVisible} onClose={toggleCartVisibility} />
