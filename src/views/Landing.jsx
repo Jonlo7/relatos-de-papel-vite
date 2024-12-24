9378ca8ac2449e161eb4c125c9b006edff160f36
Jon Lopez <jonlopez@opendeusto.es>
2024-12-24 17:38:10 +0100

    fix: landing time

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import useRedirection from "../hooks/useRedirection";
 
 export const Landing = () => {
-    useRedirection("/books", 3000); // Redireccionamo en 3 segundos
+    useRedirection("/books", 5000); // Redireccionamo en 3 segundos
 
     return (
         <div className="landing">
