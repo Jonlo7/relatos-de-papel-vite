a289caca11744a5df6894e682dadd9aec40733d8
Jon Lopez <jonlopez@opendeusto.es>
2024-12-24 17:53:08 +0100

    style: notFound and landing views

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import useRedirection from "../hooks/useRedirection";
 
+import "../styles/Landing.css";
+
 export const Landing = () => {
     useRedirection("/books", 5000); // Redireccionamo en 3 segundos
 
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "../styles/NotFound.css";
+
 export const NotFound = () => {
     return (
         <div>
             <h1>404 Not Found</h1>
-            <Link to="/">Volver a la página principal</Link>
+            <Link to="/">
+                <button className="not-found-button">Volver a la página principal</button>
+            </Link>
         </div>
     );
 }
