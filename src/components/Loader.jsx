f72a8e3708ea639df1974582155438922abd4904
Jon Lopez <jonlopez@opendeusto.es>
2024-12-25 23:35:29 +0100

    feat: loading animation

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../styles/Loader.css';
+
+export const Loader = () => (
+  <div className="loader">
+    <div className="track"></div>
+    <div className="orb"></div>
+  </div>
+);
+
+
