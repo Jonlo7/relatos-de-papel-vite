7038ba83f9711eeb5a4998f2d8603b7fa38cfe0e
Jon Lopez <jonlc2001@gmail.com>
2025-02-18 00:17:39 +0100

    test

--- tests/steps/bookDetailToCart.steps.cjs
+++ tests/steps/bookDetailToCart.steps.cjs
@@ -1,11 +1,12 @@
+// bookDetailToCart.steps.cjs
 const { When } = require('@cucumber/cucumber');
-const { getDriver } = require('./commonSteps.cjs');
 const { By, until } = require('selenium-webdriver');
+const { getDriver } = require('./commonSteps.cjs');
 
 When('el usuario hace clic en el libro {string}', async function (bookTitle) {
   const driver = getDriver();
   const bookElement = await driver.findElement(
-    By.xpath(`//div[contains(@class, "book-details")]/h2[text()="${bookTitle}"]`)
+    By.xpath(`//div[contains(@class, "book-details")]//h2[text()="${bookTitle}"]`)
   );
   await bookElement.click();
 });
@@ -21,10 +22,17 @@ When('el usuario hace clic en el botón "Añadir al carrito" en la vista de deta
 
 When('el usuario incrementa la cantidad del libro {string} en {int}', async function (bookTitle, increment) {
   const driver = getDriver();
-  const cartItemLocator = By.xpath(`//div[contains(@class, "cart-item-details")]//h2[normalize-space(text())="${bookTitle}"]/ancestor::div[contains(@class, "cart-item")]`);
+  const cartItemLocator = By.xpath(`
+    //div[contains(@class, "cart-item-details")]
+    //h2[normalize-space(text())="${bookTitle}"]
+    /ancestor::div[contains(@class, "cart-item")]
+  `);
   await driver.wait(until.elementLocated(cartItemLocator), 5000);
   const cartItem = await driver.findElement(cartItemLocator);
-  const plusButton = await cartItem.findElement(By.css('.plus-button'));
+
+  const plusButton = await cartItem.findElement(
+    By.xpath('.//div[contains(@class, "cart-quantity")]//button[contains(@class, "plus-button")]')
+  );
   for (let i = 0; i < increment; i++) {
     await plusButton.click();
     await driver.sleep(500);
